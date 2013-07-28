6b45e98fbc73af419da88c81a14eb9ae07135280
Davide D'Agostino <info@daddye.it>
2013-07-28 01:36:47 -0700

    [lthread] Use ptr since memory is alloc by lthread
    //thanks @araq

--- lt.nim
+++ lt.nim
@@ -15,10 +15,10 @@ type
 
 {.push header: "../deps/lthread/src/lthread.h", importc.}
 proc lthread_summary* (): cstring
-proc lthread_create* (new_lt: ref ref LThread, a3: LThreadFunc, arg: pointer): cint
-proc lthread_cancel* (lt: ref LThread)
+proc lthread_create* (new_lt: var ptr LThread, a3: LThreadFunc, arg: pointer): cint
+proc lthread_cancel* (lt: ptr LThread)
 proc lthread_run* ()
-proc lthread_join* (lt: ref LThread, p: ref pointer, timeout: uint64): cint
+proc lthread_join* (lt: ptr LThread, p: ref pointer, timeout: uint64): cint
 proc lthread_detach* ()
 proc lthread_sleep* (msecs: uint64)
 proc lthread_wakeup* (lt: ref LThread)
@@ -29,7 +29,7 @@ proc lthread_cond_broadcast* (c: ref LThreadCond)
 proc lthread_init* (size: int): cint
 proc lthread_get_data* (): pointer
 proc lthread_set_data* (data: pointer)
-proc lthread_current* (): ref LThread
+proc lthread_current* (): ptr LThread
 
 # socket related functions
 proc lthread_socket* (a2: cint, a3: cint, a4: cint): cint
@@ -86,19 +86,19 @@ when isMainModule:
     proc c (x: pointer){.cdecl.} =
       echo "c is running"
       discard lthread_compute_begin()
-      discard sleep(1)
+      discard sleep(8)
       lthread_compute_end()
       echo "c is exiting"
 
     proc d (x: pointer){.cdecl.} =
-      var lt_new: ref ref LThread = nil; lt_new.new
+      var lt_new: ptr LThread
       lthread_detach()
       echo "d is running"
       echo lthread_create(lt_new, c, nil)
-      echo lthread_join(lt_new[], nil, 0)
+      echo lthread_join(lt_new, nil, 0)
       echo "d is done joining on c."
 
-    var t: ref ref LThread = nil; t.new
+    var t: ptr LThread
     discard lthread_create(t, a, nil)
     discard lthread_create(t, b, nil)
     discard lthread_create(t, d, nil)
